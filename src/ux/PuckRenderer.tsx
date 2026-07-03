5c94887cbb0a07e83ad917ac6fa91e51d35df8c2
Ilker Arkon <ilketaskim@gmail.com>
2026-07-03 21:50:34 +0200

    Fix: scroll to top on page navigation between sub-pages

--- src/ux/PuckRenderer.tsx
+++ src/ux/PuckRenderer.tsx
@@ -1,8 +1,16 @@
 "use client";
 
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 import { Render } from "@puckeditor/core";
 import { config } from "@/puck.config";
 
 export function PuckRenderer({ data }: { data: any }) {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "instant" });
+  }, [pathname]);
+
   return <Render config={config} data={data} />;
 }
